bf297ad281da9b273e231ab8a43694d59e075afc
Glenn Bacon <glenn@glennbacon.com>
2016-06-23 20:09:53 -0400

    Fix pixel and add manipulatePixel

--- scripts/worker.js
+++ scripts/worker.js
@@ -3,7 +3,7 @@ importScripts('imageManips.js');
 this.onmessage = function(e) {
   var imageData = e.data.imageData;
   var type = e.data.type;
-  var pixel = manipulate(type, r, g, b, a);
+  var manipulatePixel = getManipFunc(type);
   try {
     length = imageData.data.length / 4;
     for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
@@ -11,6 +11,7 @@ this.onmessage = function(e) {
       g = imageData.data[i * 4 + 1];
       b = imageData.data[i * 4 + 2];
       a = imageData.data[i * 4 + 3];
+      pixel = manipulatePixel(r, g, b, a);
       imageData.data[i * 4 + 0] = pixel[0];
       imageData.data[i * 4 + 1] = pixel[1];
       imageData.data[i * 4 + 2] = pixel[2];
@@ -21,8 +22,8 @@ this.onmessage = function(e) {
     function ManipulationException(message) {
       this.name = "ManipulationException";
       this.message = message;
-    };
+    }
     throw new ManipulationException('Image manipulation error');
     postMessage(undefined);
   }
-}
+};
