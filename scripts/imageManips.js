35da7d92093d86d864afb4f6fb3ce027b7ca20de
Glenn Bacon <glenn@glennbacon.com>
2016-06-23 20:28:35 -0400

    Revert changes back to working demo

--- scripts/imageManips.js
+++ scripts/imageManips.js
@@ -1,6 +1,6 @@
  // Image manipulation logic from github.com/jwill/psychic-lana
 
-function getManipFunc(type) {
+function manipulate(type, r, g, b, a) {
 
   var func = function() {};
 
@@ -9,7 +9,7 @@ function getManipFunc(type) {
     g = 255 - g;
     b = 255 - b;
     return [r, g, b, a];
-  }
+  };
 
   function makePixelChroma(r, g, b, a) {
     var max;
@@ -19,7 +19,7 @@ function getManipFunc(type) {
     } else {
       return [r, g, b, a];
     }
-  }
+  };
 
   function makePixelGreyScale(r, g, b, a) {
     var y;
@@ -28,7 +28,7 @@ function getManipFunc(type) {
     g = y;
     b = y;
     return [r, g, b, a];
-  }
+  };
 
   function makePixelVibrant(r, g, b, a) {
     var amt, avg, bs, gs, mx, rs;
@@ -39,7 +39,7 @@ function getManipFunc(type) {
     gs = g + (amt * (mx - g));
     bs = b + (amt * (mx - b));
     return [rs, gs, bs, a];
-  }
+  };
 
   switch (type) {
     case "invert":
@@ -59,5 +59,5 @@ function getManipFunc(type) {
       break;
   }
 
-  return func;
+  return func(r, g, b, a);
 }
--- scripts/worker.js
+++ scripts/worker.js
@@ -6,13 +6,12 @@ this.onmessage = function(e) {
 
   try {
     length = imageData.data.length / 4;
-    var manipulatePixel = getManipFunc(type);
     for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
       r = imageData.data[i * 4 + 0];
       g = imageData.data[i * 4 + 1];
       b = imageData.data[i * 4 + 2];
       a = imageData.data[i * 4 + 3];
-      pixel = manipulatePixel(r, g, b, a);
+      pixel = manipulate(type, r, g, b, a);
       imageData.data[i * 4 + 0] = pixel[0];
       imageData.data[i * 4 + 1] = pixel[1];
       imageData.data[i * 4 + 2] = pixel[2];
@@ -23,8 +22,8 @@ this.onmessage = function(e) {
     function ManipulationException(message) {
       this.name = "ManipulationException";
       this.message = message;
-    }
+    };
     throw new ManipulationException('Image manipulation error');
     postMessage(undefined);
   }
-};
+}
